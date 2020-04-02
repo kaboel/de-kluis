26b6c9408f591556a0608623b7c49fe8493cc8ef
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-02 13:58:20 +0700

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "commonjs": true,
+        "node": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+    },
+    "rules": {
+        "no-console": 'off'
+    }
+};
